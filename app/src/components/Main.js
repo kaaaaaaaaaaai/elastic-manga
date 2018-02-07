945549d68f8f4b3d74b37c220a3e3028e854cf5b
kai ogita <kai-ogita@rich.co.jp>
2018-02-07 18:50:43 +0900

    master:Main containerのjs追加

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,27 +1,156 @@
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import { FormControl, FormLabel, FormControlLabel } from 'material-ui/Form';
+import Grid from 'material-ui/Grid';
+import Radio, { RadioGroup } from 'material-ui/Radio';
+import Paper from 'material-ui/Paper';
 
-
-const styles = {
+const styles = theme => ({
     root: {
-        width: '100%',
+        flexGrow: 1,
     },
-};
+    demo: {
+        height: 240,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        height: '100%',
+    },
+    control: {
+        padding: theme.spacing.unit * 2,
+    },
+});
+
 
 class Main extends Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            direction: 'row',
+            justify: 'center',
+            alignItems: 'center',
+        };
+        this.handleChange = this.handleChange.bind(this)
     }
+
+    handleChange = key => (event, value) => {
+        this.setState({
+            [key]: value,
+        });
+    };
+
 
     render() {
         const { classes } = this.props;
+        const { alignItems, direction, justify } = this.state;
         return (
-            <div>
-                {/*search bar*/}
-
-                {/* result box */}
-            </div>
+            <Grid container className={classes.root}>
+                <Grid item xs={12}>
+                    <Grid
+                        container
+                        className={classes.demo}
+                        alignItems={alignItems}
+                        direction={direction}
+                        justify={justify}
+                    >
+                        {[0, 1, 2].map(value => (
+                            <Grid key={value} item>
+                                <Paper
+                                    className={classes.paper}
+                                    style={{ paddingTop: (value + 1) * 10, paddingBottom: (value + 1) * 10 }}
+                                >
+                                    {`Cell ${value + 1}`}
+                                </Paper>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.control}>
+                        <Grid container>
+                            <Grid item xs={6} sm={4}>
+                                <FormControl component="fieldset">
+                                    <FormLabel>direction</FormLabel>
+                                    <RadioGroup
+                                        name="direction"
+                                        aria-label="direction"
+                                        value={direction}
+                                        onChange={this.handleChange('direction')}
+                                    >
+                                        <FormControlLabel value="row" control={<Radio />} label="row" />
+                                        <FormControlLabel value="row-reverse" control={<Radio />} label="row-reverse" />
+                                        <FormControlLabel value="column" control={<Radio />} label="column" />
+                                        <FormControlLabel
+                                            value="column-reverse"
+                                            control={<Radio />}
+                                            label="column-reverse"
+                                        />
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} sm={4}>
+                                <FormControl component="fieldset">
+                                    <FormLabel>justify</FormLabel>
+                                    <RadioGroup
+                                        name="justify"
+                                        aria-label="justify"
+                                        value={justify}
+                                        onChange={this.handleChange('justify')}
+                                    >
+                                        <FormControlLabel value="flex-start" control={<Radio />} label="flex-start" />
+                                        <FormControlLabel value="center" control={<Radio />} label="center" />
+                                        <FormControlLabel value="flex-end" control={<Radio />} label="flex-end" />
+                                        <FormControlLabel
+                                            value="space-between"
+                                            control={<Radio />}
+                                            label="space-between"
+                                        />
+                                        <FormControlLabel
+                                            value="space-around"
+                                            control={<Radio />}
+                                            label="space-around"
+                                        />
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} sm={4}>
+                                <FormControl component="fieldset">
+                                    <FormLabel>alignItems</FormLabel>
+                                    <RadioGroup
+                                        name="alignItems"
+                                        aria-label="alignItems"
+                                        value={alignItems}
+                                        onChange={this.handleChange('alignItems')}
+                                    >
+                                        <FormControlLabel value="flex-start" control={<Radio />} label="flex-start" />
+                                        <FormControlLabel value="center" control={<Radio />} label="center" />
+                                        <FormControlLabel value="flex-end" control={<Radio />} label="flex-end" />
+                                        <FormControlLabel value="stretch" control={<Radio />} label="stretch" />
+                                        <FormControlLabel value="baseline" control={<Radio />} label="baseline" />
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
+            // <div>
+            //     {/*search bar*/}
+            //     <div>
+            //         <FormControl fullWidth className={classes.formControl}>
+            //             <InputLabel htmlFor="amount">Amount</InputLabel>
+            //             <Input
+            //                 id="adornment-amount"
+            //                 value="aaa"
+            //                 onChange="aaa"
+            //                 startAdornment={<InputAdornment position="start">ケンサクケンサクゥ</InputAdornment>}
+            //             />
+            //         </FormControl>
+            //     </div>
+            //     {/* result box */}
+            // </div>
         );
     }
 }

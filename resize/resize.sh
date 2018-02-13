4f0028b58cf4be2e09a9d1dbf72993deeb38ae0a
kai ogita <kai@kainoMacBook-Pro.local>
2018-02-13 22:38:21 +0900

    fix

--- resize/resize.sh
+++ resize/resize.sh
@@ -5,5 +5,11 @@ for file in `ls ./input/`; do
 	WIDTH=$(sips -g pixelHeight -g pixelWidth ./input/${file} | grep pixelWidth | awk '{print $2}')
 	echo $HEIGHT;
 	echo $WIDTH;
-
+	if [ $HEIGHT -gt $WIDTH ]; then
+	    echo "縦長"
+        ffmpeg -y -i ./input/${file} -vf "pad=${HEIGHT}:0:(ow-iw)/2:0:white" ./output/${file}
+    else
+        echo "横長"
+        ffmpeg -y -i ./input/${file} -vf "pad=0:${WIDTH}:0:(oh-ih)/2:white" ./output/${file}
+    fi
 done
